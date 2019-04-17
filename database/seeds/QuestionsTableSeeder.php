bb5de796c96549c32c4d26e9539de0645dd3aa3d
ajm <ajm257@njit.edu>
2019-04-16 21:39:12 -0400

    Added question migration, model, and updated the Questions seeder to create questions. Added User test - one function for inserting new users (should have been included in userProfile branch) and one function to check questions are returned/retrieved. Added Question test.

---
+++ database/seeds/QuestionsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class QuestionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = App\User::all(); //retrieves all users
+        $users->each(function ($user) { //make 1 question per user
+            $question = factory(\App\Question::class)->make(); //make a question
+            $question->user()->associate($user); //associate user with a question
+            $question->save();
+        });
+    }
+}
