3613beb077fbd1789d119eaac2b7dcb75130a3f6
ajm <ajm257@njit.edu>
2019-04-17 21:45:35 -0400

    Added list of questions on the homepage and a pager.

--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -12,10 +12,12 @@ class QuestionsTableSeeder extends Seeder
     public function run()
     {
         $users = App\User::all(); //retrieves all users
-        $users->each(function ($user) { //make 1 question per user
-            $question = factory(\App\Question::class)->make(); //make a question
-            $question->user()->associate($user); //associate user with a question
-            $question->save();
-        });
+        for ($i = 1; $i <= 16; $i++) {
+            $users->each(function ($user) { //make 1 question per user
+                $question = factory(\App\Question::class)->make(); //make a question
+                $question->user()->associate($user); //associate user with a question
+                $question->save();
+            });
+        }
     }
 }
